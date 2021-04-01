5a74dc5d7434835f409e141bc5d0efa04618a870
Sam <Izguerra.sam@gmail.com>
2021-04-01 15:59:41 -0500

    added user create action

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -8,5 +8,17 @@ class Api::RecipesController < ApplicationController
     @recipe = Recipe.find_by(id: params[:id])
     render 'show.json.jb'
   end
-  
+
+  def create
+    @recipe = Recipe.new(
+      chef: params[:chef],
+      title: params[:title],
+      prep_time: params[:prep_time],
+      ingredients: params[:ingredients],
+      directions: params[:directions],
+      image_url: params[:image_url],
+    )
+    @recipe.save
+    render 'show.json.jb'
+  end
 end

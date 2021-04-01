16aa3f194cba24abc78a85ef488ebf98661833a7
Sam <Izguerra.sam@gmail.com>
2021-04-01 16:44:20 -0500

    added user update action

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -21,4 +21,16 @@ class Api::RecipesController < ApplicationController
     @recipe.save
     render 'show.json.jb'
   end
+
+  def update
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.chef = params[:chef] || @recipe.chef
+    @recipe.title = params[:title] || @recipe.title
+    @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+    @recipe.directions = params[:directions] || @recipe.directions
+    @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+    @recipe.image_url = params[:image_url] || @recipe.image_url
+    @recipe.save
+    render 'show.json.jb'
+  end
 end
